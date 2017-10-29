1a8f9f075fdff5ce982acc50c3e88ffd92f361ea
Lut <wakawaka2610@gmail.com>
2017-10-29 17:22:23 +0700

    Create Texture

---
+++ Metroid_1986/Texture.h
@@ -0,0 +1,15 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#pragma once
+#include <windows.h>
+#include <windowsx.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+
+
+class Texture{
+
+};
+
+#endif TEXTURE_H
